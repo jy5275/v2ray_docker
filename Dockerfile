fc954f25a377a7c7101ef2ad2f1cc19e828d6cf4
jy5275 <527507046@qq.com>
2021-06-23 00:40:47 +0800

    mod config file

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
-FROM --platform=${TARGETPLATFORM} alpine:latest
+FROM alpine:latest
 LABEL maintainer "V2Fly Community <dev@v2fly.org>"
 
 WORKDIR /root
-ARG TARGETPLATFORM
-ARG TAG
+# ARG TARGETPLATFORM
+# ARG TAG
 COPY v2ray.sh /root/v2ray.sh
+COPY config_new.json /root/config_new.json
 
 RUN set -ex \
 	&& apk add --no-cache tzdata openssl ca-certificates \
 	&& mkdir -p /etc/v2ray /usr/local/share/v2ray /var/log/v2ray \
 	&& chmod +x /root/v2ray.sh \
-	&& /root/v2ray.sh "${TARGETPLATFORM}" "${TAG}"
+	&& /root/v2ray.sh
 
 VOLUME /etc/v2ray
 CMD [ "/usr/bin/v2ray", "-config", "/etc/v2ray/config.json" ]
--- v2ray.sh
+++ v2ray.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
 # Set ARG
-PLATFORM=$1
-TAG=$2
-if [ -z "$PLATFORM" ]; then
-    ARCH="64"
-else
-    case "$PLATFORM" in
-        linux/386)
-            ARCH="32"
-            ;;
-        linux/amd64)
-            ARCH="64"
-            ;;
-        linux/arm/v6)
-            ARCH="arm32-v6"
-            ;;
-        linux/arm/v7)
-            ARCH="arm32-v7a"
-            ;;
-        linux/arm64|linux/arm64/v8)
-            ARCH="arm64-v8a"
-            ;;
-        *)
-            ARCH=""
-            ;;
-    esac
-fi
-[ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
+# PLATFORM=$1
+# TAG=$2
+# if [ -z "$PLATFORM" ]; then
+#     ARCH="64"
+# else
+#     case "$PLATFORM" in
+#         linux/386)
+#             ARCH="32"
+#             ;;
+#         linux/amd64)
+#             ARCH="64"
+#             ;;
+#         linux/arm/v6)
+#             ARCH="arm32-v6"
+#             ;;
+#         linux/arm/v7)
+#             ARCH="arm32-v7a"
+#             ;;
+#         linux/arm64|linux/arm64/v8)
+#             ARCH="arm64-v8a"
+#             ;;
+#         *)
+#             ARCH=""
+#             ;;
+#     esac
+# fi
+# [ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 
+ARCH=64
+TAG=v4.41.0
 # Download files
 V2RAY_FILE="v2ray-linux-${ARCH}.zip"
 DGST_FILE="v2ray-linux-${ARCH}.zip.dgst"
@@ -58,7 +60,8 @@ echo "Prepare to use"
 unzip v2ray.zip && chmod +x v2ray v2ctl
 mv v2ray v2ctl /usr/bin/
 mv geosite.dat geoip.dat /usr/local/share/v2ray/
-mv config.json /etc/v2ray/config.json
+# mv config.json /etc/v2ray/config.json
+mv /root/config_new.json /etc/v2ray/config.json
 
 # Clean
 rm -rf ${PWD}/*
